c2d006017973b70fdfbd23cf8b30f651b08dd169
Rsangram007 <sangrambeheraa007@gmail.com>
2024-05-28 11:25:51 +0530

    done

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 4000;
+const openaiRoutes = require("./routes/openaiRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/", (req, res) => {
+  res.send("Hello");
+});
+app.use("/api", openaiRoutes);
+
+
+// Start server
+app.listen(port, () => console.log(`Server running on port ${port}`));
