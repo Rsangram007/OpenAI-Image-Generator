9f821eb32452290c3b4b6f9b92339a05cec0508f
Rsangram007 <sangrambeheraa007@gmail.com>
2024-05-28 11:52:20 +0530

    done

--- Test/index.js
+++ Test/index.js
@@ -2,4 +2,8 @@ const express=require('express')
 
 const app = express()
 
-app.use()
+app.use("/",(req,res)=>{
+res.send("console.log Server Running")
+})
+
+app.listen(3000,console.log("sever is running" + 3000))
