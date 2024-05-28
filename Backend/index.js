1c05e7ca8014aef9c5f2592795153ff2214b9712
Rsangram007 <sangrambeheraa007@gmail.com>
2024-05-28 12:17:10 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,21 @@
-const cors = require("cors");
+// const cors = require("cors");
 const express = require("express");
-const dotenv = require("dotenv").config();
+// const dotenv = require("dotenv").config();
 const port = process.env.PORT || 4000;
-const openaiRoutes = require("./routes/openaiRoutes");
+// const openaiRoutes = require("./routes/openaiRoutes");
 
 const app = express();
 
 // Middleware
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/", (req, res) => {
   res.send("Hello");
 });
-app.use("/api", openaiRoutes);
+// app.use("/api", openaiRoutes);
 
 
 // Start server
