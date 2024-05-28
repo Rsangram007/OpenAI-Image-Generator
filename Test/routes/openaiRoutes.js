c3cadd1c207c2f3b8f280f13b6a7f7894f48f150
Rsangram007 <sangrambeheraa007@gmail.com>
2024-05-28 13:04:52 +0530

    done

--- Test/routes/openaiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const openaiController = require("../controllers/openaiController");
-
-// Routes
-router.post("/generateImage", openaiController.generateImage);
-
-module.exports = router;
